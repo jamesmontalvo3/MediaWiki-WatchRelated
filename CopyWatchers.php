774ddc10769082698fc5ee192704459b9fb6838b
James Montalvo <jamesmontalvo3@gmail.com>
2017-04-24 09:22:27 -0500

    Make work in MW 1.27 (#3)

--- CopyWatchers.php
+++ CopyWatchers.php
@@ -1,67 +1,15 @@
 <?php
-/** 
- * The CopyWatchers extension allows editors to copy watchers from other pages
- * using the parser function #copywatchers.
- * 
- * Documentation: http://???
- * Support:       http://???
- * Source code:   http://???
- *
- * @file CopyWatchers.php
- * @addtogroup Extensions
- * @author James Montalvo
- * @copyright © 2013 by James Montalvo
- * @licence GNU GPL v3+
- */
-
-# Not a valid entry point, skip unless MEDIAWIKI is defined
-if (!defined('MEDIAWIKI')) {
-	die( "CopyWatchers extension" );
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'CopyWatchers' );
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['CopyWatchers'] = __DIR__ . '/i18n';
+	$wgExtensionMessagesFiles['CopyWatchersMagic'] = __DIR__ . '/Magic.php';
+	wfWarn(
+		'Deprecated PHP entry point used for FooBar extension. ' .
+		'Please use wfLoadExtension instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	);
+	return;
+} else {
+	die( 'This version of the CopyWatchers extension requires MediaWiki 1.25+' );
 }
-
-$wgExtensionCredits['specialpage'][] = array(
-	'path'           => __FILE__,
-	'name'           => 'Copy Watchers',
-	'url'            => 'http://www.mediawiki.org/wiki/Extension:CopyWatchers',
-	'author'         => 'James Montalvo',
-	'descriptionmsg' => 'copywatchers-desc',
-	'version'        => '0.1.0'
-);
-
-$dir = dirname( __FILE__ ) . '/';
-$wgExtensionMessagesFiles['CopyWatchers'] = $dir . 'CopyWatchers.i18n.php';
-$wgAutoloadClasses['CopyWatchers'] = $dir . 'CopyWatchers.body.php';
-
-// Specify the function that will initialize the parser function.
-$wgHooks['ParserFirstCallInit'][] = 'CopyWatchers::setup';
-
-
-/*
-$wgAvailableRights[] = 'semanticwatch';
-$wgAvailableRights[] = 'semanticwatchgroups';
-
-$wgHooks['LoadExtensionSchemaUpdates'][] = 'SWLHooks::onSchemaUpdate';
-$wgHooks['SMWStore::updateDataBefore'][] = 'SWLHooks::onDataUpdate';
-$wgHooks['GetPreferences'][] = 'SWLHooks::onGetPreferences';
-$wgHooks['UserSaveOptions'][] = 'SWLHooks::onUserSaveOptions';
-$wgHooks['AdminLinks'][] = 'SWLHooks::addToAdminLinks';
-$wgHooks['PersonalUrls'][] = 'SWLHooks::onPersonalUrls';
-$wgHooks['SWLGroupNotify'][] = 'SWLHooks::onGroupNotify';
-
-$wgExtensionFunctions[]        = 'wfSetupCatHook';
-$wgHooks['LanguageGetMagic'][] = 'wfCatHookLanguageGetMagic';
-
-$wgHooks['ParserAfterTidy'][]  = 'wfUserPageViewTracker';
-
-class CategoryWatch {
-	function __construct() {
-		global $wgHooks;
-		$wgHooks['ArticleSave'][] = $this;
-		$wgHooks['ArticleSaveComplete'][] = $this;
-	}
-	...
-}
-
-
-
-*/
